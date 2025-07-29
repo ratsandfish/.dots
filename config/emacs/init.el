410fa907dbbd7ae2e0efc45c903e3256995d344e
ratsandfish <ratsandfish@proton.me>
2025-07-29 12:37:12 -0700

    big chungus

---
+++ config/emacs/init.el
@@ -0,0 +1,23 @@
+(org-babel-load-file
+ (expand-file-name
+  "meow.org"
+  user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(all-the-icons auto-package-update catppuccin-theme centered-window
+				   corfu ef-themes gcmh golden-ratio
+				   gruber-darker-theme keycast modus-themes orderless
+				   org-superstar pdf-tools sudo-edit swiper
+				   vertico-posframe))
+ '(package-vc-selected-packages
+   '((denote-search :url "https://github.com/lmq-10/denote-search"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
